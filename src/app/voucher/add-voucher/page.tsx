ff57a34a4de0ab3c03fadd2bea5e8043dcead824
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-06-03 01:57:07 +0500

    voucher created, list and siable work done

---
+++ src/app/voucher/add-voucher/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import VoucherForm from "@/components/Forms/VoucherForm";
+
+const page = () => {
+  return (
+    <DefaultLayout>
+      <VoucherForm />
+    </DefaultLayout>
+  );
+};
+
+export default page;
