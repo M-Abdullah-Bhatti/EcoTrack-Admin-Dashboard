d6a9e5152f09b668006cfbcb0d8e6674232c9eaf
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2024-06-12 15:44:45 +0500

    company work initialized

---
+++ src/app/buy-credits/page.tsx
@@ -0,0 +1,13 @@
+import PaymentForm from "@/components/Forms/PaymentForm";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React from "react";
+
+const page = () => {
+  return (
+    <DefaultLayout>
+      <PaymentForm />
+    </DefaultLayout>
+  );
+};
+
+export default page;
--- src/components/Sidebar/CompanyLinks.tsx
+++ src/components/Sidebar/CompanyLinks.tsx
@@ -56,9 +56,9 @@ const CompanyLinks = () => {
         {/* <!-- Menu Item Calendar --> */}
         <li>
           <Link
-            href="/calendar"
+            href="/buy-credits"
             className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-              pathname.includes("calendar") && "bg-graydark dark:bg-meta-4"
+              pathname.includes("buy-credits") && "bg-graydark dark:bg-meta-4"
             }`}
           >
             <svg
@@ -74,7 +74,7 @@ const CompanyLinks = () => {
                 fill=""
               />
             </svg>
-            Calendar
+            Buy Credits
           </Link>
         </li>
         {/* <!-- Menu Item Calendar --> */}
